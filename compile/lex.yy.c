b7a9fbb31be6dbdbd431920db28a0d637307928f
jiuxiang <jiuxiang.deng@igg.com>
2025-02-18 10:10:07 +0000

    简单计算器

--- compile/lex.yy.c
+++ compile/lex.yy.c
@@ -354,8 +354,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 12
-#define YY_END_OF_BUFFER 13
+#define YY_NUM_RULES 13
+#define YY_END_OF_BUFFER 14
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -363,10 +363,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[21] =
+static const flex_int16_t yy_accept[19] =
     {   0,
-        0,    0,   13,   11,    1,    1,    9,   10,    7,    5,
-        6,    8,    2,    4,    1,    3,    2,    4,    3,    0
+        0,    0,   14,   12,   11,    9,    6,    7,    3,    1,
+        2,    4,    8,    5,   10,    8,   10,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -375,16 +375,16 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    2,    1,    1,    1,    1,    1,    1,    1,    4,
-        5,    6,    7,    1,    8,    9,   10,   11,   11,   11,
-       11,   11,   11,   11,   11,   11,   11,    1,    1,    1,
-        1,    1,    1,    1,   12,   12,   12,   12,   12,   12,
-       12,   12,   12,   12,   12,   12,   12,   12,   12,   12,
-       12,   12,   12,   12,   12,   12,   12,   12,   12,   12,
-        1,    1,    1,    1,   12,    1,   12,   12,   12,   12,
+        5,    6,    7,    1,    8,    1,    9,   10,   10,   10,
+       10,   10,   10,   10,   10,   10,   10,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
 
-       12,   12,   12,   12,   12,   12,   12,   12,   12,   12,
-       12,   12,   12,   12,   12,   12,   12,   12,   12,   12,
-       12,   12,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,   11,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -401,40 +401,36 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[13] =
+static const YY_CHAR yy_meta[12] =
     {   0,
-        1,    2,    2,    1,    1,    1,    1,    1,    1,    1,
-        3,    3
+        1,    1,    2,    1,    1,    1,    1,    1,    1,    1,
+        1
     } ;
 
-static const flex_int16_t yy_base[23] =
+static const flex_int16_t yy_base[20] =
     {   0,
-        0,    0,   19,   20,    0,    0,   20,   20,   20,   20,
-       20,   20,    4,    0,    0,    7,    0,    0,    6,   20,
-       14,   11
+        0,    0,   16,   17,   17,   17,   17,   17,   17,   17,
+       17,    6,    4,   17,    0,    3,    0,   17,   11
     } ;
 
-static const flex_int16_t yy_def[23] =
+static const flex_int16_t yy_def[20] =
     {   0,
-       20,    1,   20,   20,   21,   21,   20,   20,   20,   20,
-       20,   20,   20,   22,   21,   20,   13,   22,   20,    0,
-       20,   20
+       18,    1,   18,   18,   18,   18,   18,   18,   18,   18,
+       18,   18,   18,   18,   19,   18,   19,    0,   18
     } ;
 
-static const flex_int16_t yy_nxt[33] =
+static const flex_int16_t yy_nxt[29] =
     {   0,
-        4,    5,    6,    7,    8,    9,   10,   11,    4,   12,
-       13,   14,   16,   18,   17,   15,   19,   19,   20,    3,
-       20,   20,   20,   20,   20,   20,   20,   20,   20,   20,
-       20,   20
+        4,    5,    6,    7,    8,    9,   10,   11,   12,   13,
+       14,   17,   16,   16,   15,   18,    3,   18,   18,   18,
+       18,   18,   18,   18,   18,   18,   18,   18
     } ;
 
-static const flex_int16_t yy_chk[33] =
+static const flex_int16_t yy_chk[29] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,   13,   22,   13,   21,   19,   16,    3,   20,
-       20,   20,   20,   20,   20,   20,   20,   20,   20,   20,
-       20,   20
+        1,   19,   16,   13,   12,    3,   18,   18,   18,   18,
+       18,   18,   18,   18,   18,   18,   18,   18
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -453,10 +449,9 @@ int yy_flex_debug = 0;
 char *yytext;
 #line 1 "calc.l"
 #line 2 "calc.l"
-#include <stdio.h>
-#line 457 "lex.yy.c"
-/* 正则表达式定义（可选） */
-#line 459 "lex.yy.c"
+    #include "calc.tab.h"
+#line 453 "lex.yy.c"
+#line 454 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -673,14 +668,9 @@ YY_DECL
 		}
 
 	{
-#line 9 "calc.l"
+#line 6 "calc.l"
 
-#line 11 "calc.l"
-    /* 规则部分：模式 + 动作 */
-    /* ---- 注意：规则必须顶格写，前面不能有空格 ---- */
-
-    /* 忽略空白符 */
-#line 683 "lex.yy.c"
+#line 673 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -707,13 +697,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 21 )
+				if ( yy_current_state >= 19 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 20 );
+		while ( yy_base[yy_current_state] != 17 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -738,72 +728,72 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
-/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 15 "calc.l"
-{ /* 无动作 */ }
+#line 7 "calc.l"
+{ return ADD; }
 	YY_BREAK
-/* 数字 */
 case 2:
 YY_RULE_SETUP
-#line 18 "calc.l"
-{ printf("整数: %s\n", yytext); }
+#line 8 "calc.l"
+{ return SUB; }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 19 "calc.l"
-{ printf("浮点数: %s\n", yytext); }
+#line 9 "calc.l"
+{ return MUL; }
 	YY_BREAK
-/* 变量 */
 case 4:
 YY_RULE_SETUP
-#line 22 "calc.l"
-{ printf("变量: %s\n", yytext); }
+#line 10 "calc.l"
+{ return DIV; }
 	YY_BREAK
-/* 运算符 */
 case 5:
 YY_RULE_SETUP
-#line 25 "calc.l"
-{ printf("加号: +\n"); }
+#line 11 "calc.l"
+{ return ABS; }
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 26 "calc.l"
-{ printf("减号: -\n"); }
+#line 12 "calc.l"
+{ return OP; }
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 27 "calc.l"
-{ printf("乘号: *\n"); }
+#line 13 "calc.l"
+{ return CP; }
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 28 "calc.l"
-{ printf("除号: /\n"); }
+#line 14 "calc.l"
+{ yylval = atoi(yytext); return NUMBER; }
 	YY_BREAK
-/* 括号 */
 case 9:
+/* rule 9 can match eol */
 YY_RULE_SETUP
-#line 31 "calc.l"
-{ printf("左括号\n"); }
+#line 16 "calc.l"
+{ return EOL; }
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 32 "calc.l"
-{ printf("右括号\n"); }
+#line 17 "calc.l"
+
 	YY_BREAK
-/* 错误处理 */
 case 11:
 YY_RULE_SETUP
-#line 35 "calc.l"
-{ printf("无法识别的字符: %s\n", yytext); }
+#line 18 "calc.l"
+{ /* ignore white space */ }
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 37 "calc.l"
+#line 19 "calc.l"
+{ fprintf(stderr, "Mystery character %c\n", *yytext); }
+	YY_BREAK
+case 13:
+YY_RULE_SETUP
+#line 21 "calc.l"
 ECHO;
 	YY_BREAK
-#line 806 "lex.yy.c"
+#line 796 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1100,7 +1090,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 21 )
+			if ( yy_current_state >= 19 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1128,11 +1118,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 21 )
+		if ( yy_current_state >= 19 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 20);
+	yy_is_jam = (yy_current_state == 18);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1808,10 +1798,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 37 "calc.l"
+#line 21 "calc.l"
 
-int main() {
-    yylex();
-    return 0;
-}
+
 
--- compile/readme.txt
+++ compile/readme.txt
@@ -1,17 +1,13 @@
-词法分析器
 
 
+
+
+bison -d calc.y 
 flex calc.l 
-gcc lex.yy.c -o calc
-echo "3.14 * (x + 5)" | ./calc
+gcc lex.yy.c y.tab.c -o calc
 
-输出
-浮点数: 3.14
-乘号: *
-左括号
-变量: x
-加号: +
-整数: 5
-右括号
-
+./calc
+> 3.14 * (x + 5)
+= 17.7
+> 
 
